6db8c68a24ddb634ed3237ec91df14d5246a4495
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 16:18:24 +0545

    interface in c#

---
+++ ConsoleApp/21Interface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public interface Area
+    {
+        public void area(int l, int b);
+    }
+    public interface Perimeter
+    {
+        public void perimeter(int l, int b)
+        {
+            Console.WriteLine($"Perimeter={2 * (l + b)}");
+        }
+    }
+    public class Rectangle1 : Area, Perimeter
+    {
+        public void area(int l,int b)
+        {
+            Console.WriteLine($"area={l * b}");
+        }
+    }
+    internal class _21Interface
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.perimeter(10, 7);
+            rectangle.area(10, 7);
+        }
+    }
+}
