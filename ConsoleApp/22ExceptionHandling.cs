43e71cf4ba7c12a0ce73c7607e953466d8f3721c
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 16:31:56 +0545

    Eception Handling in c#

---
+++ ConsoleApp/22ExceptionHandling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _22ExceptionHandling
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[5] { 1, 2, 3, 4, 5 };
+            int index;
+            Console.WriteLine("Enter any index:");
+            index=int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(a[index]);
+            }
+            catch (IndexOutOfRangeException e){
+            Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of Program");
+            }
+        }
+    }
+}
