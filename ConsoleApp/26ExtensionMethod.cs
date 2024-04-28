9cf677b4affbfaef5a364b9a768efff2a80dda9d
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 18:15:06 +0545

    Extension function in c#

---
+++ ConsoleApp/26ExtensionMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class Extension
+    {
+        public static bool isEven(this int n)
+        {
+            if(n%2==0)
+            {
+                return true;
+
+            }
+            return false;
+        }
+    }
+    internal class _26ExtensionMethod
+    {
+        public static void Main(string[] args)
+        {
+            int n = 30;
+            Console.Write(n.isEven());
+        }
+    }
+}
