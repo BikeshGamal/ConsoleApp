9e61e1a3379bc8f9e8cc426413a5375ef77f0f1f
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 12:38:00 +0545

    Concept of default constructor and paramaterized contructor were implemented

--- ConsoleApp/13Constructor.cs
+++ ConsoleApp/13Constructor.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+    public class Person2 {
+        public string name;
+        public int age;
+        public Person2() {
+            Console.WriteLine("This is the example of default constructor");
+        }
+        public Person2(string name,int age) {
+            this.name = name;
+            this.age = age;
+        }
+        public void showPerson()
+        {
+            Console.WriteLine("Information of person:");
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Age:"+age);
+        }
+    }
     internal class _13Constructor
     {
         public static void Main(string[] args)
         {
-
+            Person2 person = new Person2();
+            person.showPerson();
+            Person2 person2 = new Person2("Rahul Khadka",23);
+            person2.showPerson();
         }
     }
 }
