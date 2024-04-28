9adfc249f4e0a34638d9b36011a9c5b266eb8a67
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 14:28:49 +0545

    Method Overloading

---
+++ ConsoleApp/16MethodOverloading.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _16MethodOverloading
+    {
+        public static void add(int a,int b)
+        {
+            Console.WriteLine($"sum of {a} and {b}={a + b}");
+        }
+        public static void add(int a,int b,int c)
+        {
+            Console.WriteLine($"sum of {a},{b} and {c}={a + b + c}");
+        }
+        public static void add(double a, double b)
+        {
+            Console.WriteLine($"sum of {a} and {b}={a + b}");
+        }
+        public static void Main(string[] args)
+        {
+            add(5, 5);
+            add(10, 20, 30);
+            add(45.5,56.7);
+        }
+    }
+}
