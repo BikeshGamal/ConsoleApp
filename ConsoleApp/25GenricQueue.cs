6f9f625cd2378cd87660662e4295e119e0650b0d
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 18:04:48 +0545

    Generic Queue<T> in c#

---
+++ ConsoleApp/25GenricQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _25GenricQueue
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> q
+                = new Queue<string>();  
+            q.Enqueue("Raju Pathak");
+            q.Enqueue("Bhaskar Pokhrel");
+            q.Enqueue("Subechchya Chaulagain");
+            q.Enqueue("Gaurav Khadka");
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+            q.Dequeue();
+            foreach ( var item in q)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
