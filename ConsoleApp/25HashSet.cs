f52c195fc5289968fe63188b37cfce0da4141b94
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 17:59:36 +0545

    HashSet in c#

---
+++ ConsoleApp/25HashSet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _25HashSet
+    {
+        public static void Main(string[] args)
+        {
+            HashSet<int> hs=new HashSet<int>();
+            hs.Add(1);
+            hs.Add(2);
+            hs.Add(3);
+            hs.Add(4);
+            hs.Add(5);
+            foreach (int i in hs)
+            {
+                Console.WriteLine(i);
+            }
+            hs.Remove(1);
+            foreach (int i in hs)
+            { Console.WriteLine(i);}
+        }
+    }
+}
