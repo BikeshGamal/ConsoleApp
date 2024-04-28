a0f45e282f6cfbddc0e4f1ad3c25e778ba9174ac
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 17:45:41 +0545

    Stack in c#

---
+++ ConsoleApp/25Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _25Stack
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+                
+            stack.Push(5);
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            stack.Pop();
+            foreach (var item in stack)
+            { Console.WriteLine(item); }    
+        }
+    }
+}
