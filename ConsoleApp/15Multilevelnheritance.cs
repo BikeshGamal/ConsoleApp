42935a4e19c024d6aac8d8e298056b895d7cebbf
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 13:42:42 +0545

    multi level inheritance

---
+++ ConsoleApp/15Multilevelnheritance.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Person3
+    {
+        public string name;
+        public int age;
+        public Person3(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void showPerson()
+        {
+            Console.WriteLine("Information of person:");
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Age:" + age);
+
+        }
+    }
+    public class Student1:Person3
+    {
+        public int sid;
+        public Student1(int sid,string name,int age):base(name,age)
+        {
+            this.sid = sid;
+        }
+        public void showStudent()
+        {
+            Console.WriteLine("Information of student:");
+            Console.WriteLine("sid:" + sid);
+            Console.WriteLine("name:" + name);
+            Console.WriteLine("age:" + age);
+        }
+    }
+    public class CollegeStudent : Student1
+    {
+        public int cid;
+        public CollegeStudent(int cid,int sid,string name,int age) : base(sid, name, age)
+        {
+            this.cid = cid;
+        }
+        public void showCStudent()
+        {
+            Console.WriteLine("Information of college student:");
+            Console.WriteLine("cid:" + cid);
+            Console.WriteLine("sid:" + sid);
+            Console.WriteLine("name:" + name);
+            Console.WriteLine("age:" + age);
+
+        }
+    }
+    internal class _15Multilevelnheritance
+    {
+        public static void Main(string[] args)
+        {
+            CollegeStudent cs = new CollegeStudent(101, 201, "Raju Lama", 23);
+            cs.showPerson();
+            cs.showStudent();
+            cs.showCStudent();
+        }
+    }
+}
