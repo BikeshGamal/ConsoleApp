851922d2d73dafecd2b1d345e2d7c83d5f7206db
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 17:19:54 +0545

    Hashtable in c#

---
+++ ConsoleApp/25HashTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _25HashTable
+    {
+        public static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("name", "Basnata pokhrel");
+            hashtable.Add("address", "Kathmandu");
+            hashtable.Add("id", 1234);
+            foreach (var item in hashtable)
+            {
+                Console.WriteLine(item);
+            }
+            hashtable.Remove("address");
+            foreach (var item in hashtable)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
