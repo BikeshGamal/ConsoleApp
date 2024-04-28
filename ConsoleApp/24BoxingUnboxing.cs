2aceba3960b23d218443bbbe0121ab739920af7d
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 16:54:56 +0545

    Boxing and Unboxing in c#

---
+++ ConsoleApp/24BoxingUnboxing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _24BoxingUnboxing
+    {
+        public static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("a:"+a);
+            object o = a;
+            Console.WriteLine("o:"+o);
+            int b = (int)o;
+            Console.WriteLine("b:"+b);
+        }
+    }
+}
