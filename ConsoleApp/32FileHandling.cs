abebdf15543d46b1cba031a91331ee8260d23674
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 20:14:44 +0545

    file handling

---
+++ ConsoleApp/32FileHandling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _32FileHandling
+    {
+        public static void Main(string[] args)
+        {
+            string fileName = "sample.txt";
+            using (StreamWriter SW=new StreamWriter(fileName))
+            {
+                SW.WriteLine("hello");
+            }
+            using (StreamReader sr=new StreamReader(fileName))
+            {
+                string content=sr.ReadToEnd();
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
