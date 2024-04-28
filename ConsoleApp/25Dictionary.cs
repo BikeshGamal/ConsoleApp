ff459ca6813849d7c175c2b0b0cc6615fffadf23
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 17:53:28 +0545

    Dictionary in c#

---
+++ ConsoleApp/25Dictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _25Dictionary
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<String,dynamic> dc=new Dictionary<String,dynamic>();
+            dc.Add("name","Ram");
+            dc.Add("address", "KTM");
+            dc.Add("age", 23);
+            foreach(var item in dc)
+            {
+                Console.WriteLine(item);    
+            }
+            dc.Remove("age");
+            foreach(var item in dc)
+            {
+               Console.WriteLine(item);
+            }
+        }
+    }
+}
