19071fb01b5ce4a27e0647c139271fa51e1ae7f7
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 17:25:16 +0545

    Queue in c#

---
+++ ConsoleApp/25Queue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _25Queue
+    {
+        public static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            queue.Dequeue();
+            foreach (var item in queue)
+            { Console.WriteLine(item); }    
+        }
+    }
+
+}
