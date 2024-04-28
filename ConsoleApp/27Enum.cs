f546a11b157ef0dbacd308dfe6f912e2d66f7fd7
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 18:19:20 +0545

    enum in c#

---
+++ ConsoleApp/27Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _27Enum
+    {
+        public enum day {sunday,monday,tuesday,wednesday,thursday,friday,saturday }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(day.sunday);  
+            Console.WriteLine(day.monday);
+            Console.WriteLine(day.tuesday);
+            Console.WriteLine(day.wednesday);
+            Console.WriteLine(day.thursday);
+            Console.WriteLine(day.friday);
+            Console.WriteLine(day.saturday);
+        }
+    }
+}
