7e890a13a7df547a7a38898faf00c3e3b43d73c4
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 17:14:57 +0545

    ArrayList in c#

---
+++ ConsoleApp/25ArrayList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _25ArrayList
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList al=new ArrayList();
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Add(4);
+            al.Add(5);
+            foreach(int i in al)
+            {
+                Console.WriteLine(i);
+            }
+            al.Remove(3);
+            foreach(int i in al)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
