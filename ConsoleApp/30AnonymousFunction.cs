c88882fbdb8c3bdc0838562a8d9845a4b429efdd
BikeshGamal <bkssstechno@gmail.com>
2024-04-28 18:37:57 +0545

    lamda expression

---
+++ ConsoleApp/30AnonymousFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class _30AnonymousFunction
+    {
+        public static Func<int, int, int> add = (a, b) => a + b;
+        public static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("sum=" + add(10, 20));
+        }
+    }
+}
